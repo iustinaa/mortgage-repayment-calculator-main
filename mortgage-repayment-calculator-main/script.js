5a78300b0f74704fc1ad6f4d85c86c68b017b152
Iustina Bucur <bucur.iustina@outlook.com>
2024-09-24 12:02:55 +0200

    style the errar-states

--- mortgage-repayment-calculator-main/script.js
+++ mortgage-repayment-calculator-main/script.js
@@ -15,3 +15,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     });
 });
+
+document.getElementById('submit').addEventListener('click', function(event) {
+    const inputs = document.querySelectorAll('input[required]');
+    inputs.forEach(input => {
+      if (!input.checkValidity()) {
+        input.classList.add('invalid');
+      } else {
+        input.classList.remove('invalid');
+      }
+    });
+    if (!this.checkValidity()) {
+      event.preventDefault();
+    }
+  });
